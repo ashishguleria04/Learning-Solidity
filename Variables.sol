05d4d9b1911f3120095cddce2c1bdd574d00bd34
ashishguleria04 <guleriaashish65@gmail.com>
2025-01-13 00:33:22 +0530

    Variables in solidity

---
+++ Variables.sol
@@ -0,0 +1,45 @@
+// There are different types of Variables : Address, String, Uint (Integer), Bool etc
+
+pragma solidity ^0.8.0;
+
+contract Variables{
+
+    address userAddress = 0x7khwebf4734gr8fbiwqbf47gfybf7gr97; // some random keyboard strokes lol but you get the point
+
+// It can address of your crypto wallet or your smart contract or anything else. that's what  understood. 
+}
+
+
+contract Variables-string{
+
+    //It's just a string
+
+    string message = "Hello, Solidity!";
+
+}
+
+
+
+contract Variables-bool{
+
+    // Basically it's just true or false lol
+    bool isReady = true;
+
+}
+
+
+
+contract Variables-uint{
+
+    //That's integers, yeah weird name, but yeah uint
+
+    uint8 MAX_SUPPLY = 10;
+    // Small number for max supply of NFTs
+
+    uint256 WAIT-TIME = 1 days;
+    // Wait time, obviously duh... 1 day
+
+}
+
+
+// Integers are building blocks of our programs, we gonna have numbers obviously, isn't it?
