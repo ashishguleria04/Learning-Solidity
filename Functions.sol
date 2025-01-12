a8da89005e14067b617eab1bfe94285fa957fb42
ashishguleria04 <guleriaashish65@gmail.com>
2025-01-13 00:51:17 +0530

    Functions in solidity

---
+++ Functions.sol
@@ -0,0 +1,11 @@
+//Function is basically a repeatable task
+
+function get() public view returns (uint) {
+
+    return count;
+}
+
+//this above is a sort of a syntax where "get()" is function name
+// and "public" and "view" are function type
+// and "returns (uint)" is return types
+// and between "{}" these brackets, one defines their logic, I think I got the point lol
